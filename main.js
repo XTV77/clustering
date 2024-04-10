ce4d4283a196654bd1420057130f7f9ce0cf3fb8
Sergis <finayk.sergiy@gmail.com>
2024-04-10 22:00:06 +0300

    creatye module JS

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-// Отримуємо в константу елемент для виводу
-const rootElement = document.getElementById("root");
-// Виводимо (відмальовуємо) нашу розмітку
-ReactDOM.createRoot(rootElement).render(<h1></h1>);
+import A from "./var.js";
+console.log(A);
 
 const rows = 10; //кількість об'єктів
 const columns = 2; //кількість характеристик
@@ -24,14 +22,11 @@ function arrayStandartization(inputArr) {
   }
   avgInputArr /= arrLength;
   avgInputArr = parseFloat(avgInputArr.toFixed(4));
-  console.log(avgInputArr);
   for (let i = 0; i < arrLength; i++) {
     standartDeviation += (inputArr[i] - avgInputArr) ** 2;
   }
   standartDeviation = Math.sqrt(standartDeviation / (arrLength - 1)); //Некореговане стандартне відхилення (кориговане n-1)
   standartDeviation = parseFloat(standartDeviation.toFixed(4));
-  console.log(arrLength);
-  console.log(standartDeviation);
   for (let i = 0; i < arrLength; i++) {
     standartedArr[i] = parseFloat(
       ((inputArr[i] - avgInputArr) / standartDeviation).toFixed(4)
@@ -39,7 +34,22 @@ function arrayStandartization(inputArr) {
   }
   return standartedArr;
 }
-function lowestDistance() {}
+function lowestDistance(list) {
+  let lowestD = 1000;
+  let cluster = {
+    conectedObj: [],
+    distance: []
+  };
+  for (let i = 0; i < rows; i++) {
+    lowestD = 1000;
+    for (let j = 0; j < rows; j++) {
+      if (metricDistance(list[i], list[j]) < lowestD) {
+        cluster.conectedObj[i] = [i, j];
+        cluster.distance[i] = metricDistance(list[i], list[j]);
+      }
+    }
+  }
+}
 
 let price = [5500, 5000, 3200, 5500, 5500, 13000, 16600, 12200, 16000, 16000];
 let HP = [100, 130, 550, 510, 450, 450, 510, 140, 230, 250];
@@ -54,3 +64,5 @@ for (let i = 0; i < rows; i++) {
 console.log(objectList);
 console.log(metricDistance(objectList[4], objectList[3]));
 console.timeEnd("time");
+const rootElement = document.getElementById("root");
+console.log(rootElement);
---
+++ react.js
@@ -0,0 +1,4 @@
+// Отримуємо в константу елемент для виводу
+const rootElement = document.getElementById("root");
+// Виводимо (відмальовуємо) нашу розмітку
+ReactDOM.createRoot(rootElement).render(<h1>REACT</h1>);
